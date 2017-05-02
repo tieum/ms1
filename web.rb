9806700bd9e6660894a29d7e80f3c53463783d82
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-02 19:35:10 +0200

    add Dockerfile, update bootstrap

--- web.rb
+++ web.rb
@@ -1,12 +1,17 @@
+#!/usr/bin/env ruby
 require 'sinatra'
 require 'net/http'
 require 'uri'
 
+set :bind, '0.0.0.0'
+
 get '/' do
   uri = URI(ENV['MS_URL'])
-  http = Net::HTTP.new(uri.host, uri.port)
-  response = http.request(Net::HTTP::Get.new(uri.request_uri))
-  status = response.code == '200' ? 'OK', 'NOK'
-  
-  "Hello, world. I'm #{status}\n"
+#  http = Net::HTTP.new(uri.host, uri.port)
+#  response = http.request(Net::HTTP::Get.new(uri.request_uri))
+  puts "callling ms2"
+  response = Net::HTTP.get_response(uri)
+  status = response.code == '200' ? 'OK': 'NOK'
+
+  "Hello, world. I'm #{status}(#{response.code})\n"
 end
